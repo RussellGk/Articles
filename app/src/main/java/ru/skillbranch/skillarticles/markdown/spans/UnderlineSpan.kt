dc65c28ad7f851faf2b3709f6eec2de2692d4f3a
Russell <ruslan.gulenchenko@ramax.ru>
2021-10-28 13:44:24 +0300

    Fix UnderlineSpan stub

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/UnderlineSpan.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/UnderlineSpan.kt
@@ -1,4 +1,34 @@
 package ru.skillbranch.skillarticles.markdown.spans
 
-class UnderlineSpan() {
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.text.style.ReplacementSpan
+
+class UnderlineSpan(
+    private val underlineColor: Int,
+    dotWidth: Float = 6f
+) : ReplacementSpan() {
+    override fun getSize(
+        paint: Paint,
+        text: CharSequence?,
+        start: Int,
+        end: Int,
+        fm: Paint.FontMetricsInt?
+    ): Int {
+       return 0
+    }
+
+    override fun draw(
+        canvas: Canvas,
+        text: CharSequence?,
+        start: Int,
+        end: Int,
+        x: Float,
+        top: Int,
+        y: Int,
+        bottom: Int,
+        paint: Paint
+    ) {
+
+    }
 }
